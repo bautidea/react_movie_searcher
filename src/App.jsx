e97d7792c6e6f429e3286601aff56dfa5a2fb61c
bautidea <juanbautistadeangelis@gmail.com>
2024-03-22 09:23:23 -0300

    Finishing with form, and data fetching.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,45 @@
+import { useState } from 'react'
 import './App.css'
 
+const apiKey = import.meta.env.VITE_API_KEY
+const MOVIE_ENDPOINT_SEARCH = `http://www.omdbapi.com/?apikey=${apiKey}&`
+
 function App() {
+  // State to store movie name to search.
+  const [ movieToSearch, setMovieToSearch ] = useState()
+
+  useState( () => {
+    if (!movieToSearch) return 
+
+    fetch(`${MOVIE_ENDPOINT_SEARCH}t=${movieToSearch}`)
+    .then((res) => {
+      return res.json()
+    })
+    .then((data) => console.log(data))
+  }, [movieToSearch])
+
+  const handleClick = () => {
+
+  }
+
   return (
     <>
       <div className='page'>
-        <header>
-        <h1>Movie Searcher</h1>
+        <header className='header'>
+
+          <h1>Movie Searcher</h1>
+
           <form className='form'>
-            <input placeholder='Avengers, Star Wars, ...'></input>
-            <button type='submit'>Search</button>
+
+            <label className='label'>
+                <p>Movie Name:</p>
+                <input value={movieToSearch} type='text' placeholder='Avengers, Star Wars, ...'/>
+            </label>
+
+            <button type='submit' onClick={handleClick} >
+              Search
+            </button>
+
           </form>
         </header>
 
