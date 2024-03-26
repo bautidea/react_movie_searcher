3e754ee93865e140192deb6a64c781978dd13521
bautidea <juanbautistadeangelis@gmail.com>
2024-03-26 09:49:29 -0300

    Applied controlled form handling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css'
+import { useState } from 'react'
 import useMovies from './hooks/useMovies'
 import ShowMovies from './components/ShowMovies'
 
 function App() {
   const { movie } = useMovies('Star')
+  const [ value, setValue ] = useState('')
 
+  // Handling form in a 'controlled' way, because React is controlling the state.
+  // In this way its easier to perform form validation.
+  function handleSubmit (event) {
+    event.preventDefault()
+    console.log({ value });
+  }
+
+  function handleChange (event) {
+    setValue(event.target.value)
+  }
 
   return (
     <>
@@ -13,11 +25,11 @@ function App() {
 
           <h1>Movie Searcher</h1>
 
-          <form className='form'>
+          <form className='form' onSubmit={handleSubmit}>
 
             <label className='label'>
                 <p>Movie Name:</p>
-                <input type='text' placeholder='Avengers, Star Wars, ...'/>
+                <input value={value} onChange={handleChange} type='text' placeholder='Avengers, Star Wars, ...'/>
             </label>
 
             <button type='submit'>
