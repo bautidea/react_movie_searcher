c1423df98a1eac32ebc56357b53fcd72bd32a0d6
bautidea <juanbautistadeangelis@gmail.com>
2024-03-28 11:08:12 -0300

    Styling elements and applying proptypes to ShowMovies component.

--- src/components/ShowMovies.jsx
+++ src/components/ShowMovies.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 const ShowMovies = ({ mappedMovies }) => {
   const hasMovies = mappedMovies?.length > 0
 
@@ -6,29 +8,44 @@ const ShowMovies = ({ mappedMovies }) => {
       {
         hasMovies 
 
-        &&
+        ?
 
-        <ul className='movieContainer'>
-          {
-            mappedMovies.map((movie) => (
-              <li key={movie.id} className="movieItem">
-                <div className="movieInfo">
-                  <h2>{movie.title}</h2>
-                  <p>{movie.year}</p>
-                </div>
+        (
+          <ul className='movieContainer'>
+            {
+              mappedMovies.map((movie) => (
+                <li key={movie.id} className="movieItem">
+                  <img src={movie.poster} alt={`${movie.title} Poster`} className="moviePoster"/>
 
-                <img src={movie.poster} alt={`${movie.title} Poster`} className="moviePoster"/>
-              </li>
-            ))
-          }
-        </ul>
+                  <div className="movieInfo">
+                    <h2>{movie.title}</h2>
+                    <p>{movie.year}</p>
+                  </div>
+                </li>
+              ))
+            }
+          </ul>
+        )
 
-        || 
+        :
 
-        <h2>No Movies Found</h2>
+        (
+          <h2>No Movies Found</h2>
+        )
       }
     </>
   );
+};
+
+ShowMovies.propTypes = {
+  mappedMovies: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      poster: PropTypes.string.isRequired,
+      year: PropTypes.number.isRequired,
+    })
+  ),
 };
 
 export default ShowMovies;
